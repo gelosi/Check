1bde95009dd23ef5181e7d8e844a25f9af11a154
Oleg <gelosi@gmail.com>
2015-12-06 16:35:30 +0100

    Initial Commit

---
+++ Check/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Check
+//
+//  Created by Oleg Shanyuk on 12/6/15.
+//  Copyright © 2015 xaTa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Check/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Check
+//
+//  Created by Oleg Shanyuk on 12/6/15.
+//  Copyright © 2015 xaTa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
